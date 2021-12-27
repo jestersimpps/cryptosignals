9aa1533e32ef0ad2a17e9a281465093e79473588
jestersimpps <jestersimpps@gmail.com>
2021-12-27 14:35:38 +0000

    add rsi

--- server/src/main.ts
+++ server/src/main.ts
@@ -66,24 +66,28 @@ const init = async () => {
               signals.stoch.t1m.d.value,
               signals.stoch.t1m.d.angle,
               signals.stoch.t1m.cross ? (signals.stoch.t1m.cross === "UP" ? 1 : -1) : 0,
+              signals.rsi.t1m,
 
               signals.stoch.t5m.k.value,
               signals.stoch.t5m.k.angle,
               signals.stoch.t5m.d.value,
               signals.stoch.t5m.d.angle,
               signals.stoch.t5m.cross ? (signals.stoch.t1m.cross === "UP" ? 1 : -1) : 0,
+              signals.rsi.t5m,
 
               signals.stoch.t15m.k.value,
               signals.stoch.t15m.k.angle,
               signals.stoch.t15m.d.value,
               signals.stoch.t15m.d.angle,
               signals.stoch.t15m.cross ? (signals.stoch.t1m.cross === "UP" ? 1 : -1) : 0,
+              signals.rsi.t15m,
 
               signals.stoch.t1h.k.value,
               signals.stoch.t1h.k.angle,
               signals.stoch.t1h.d.value,
               signals.stoch.t1h.d.angle,
               signals.stoch.t1h.cross ? (signals.stoch.t1m.cross === "UP" ? 1 : -1) : 0,
+              signals.rsi.t1h,
             ];
             aiService.addDataRow(inputs, candlesObject.t1m);
           },
